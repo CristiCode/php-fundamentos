4e567d1c07424f8edb9b19495429431f97d1dfb0
CristiCode <cristicode12@gmail.com>
2023-11-15 17:13:56 +0100

    [+] Operadores

--- operadores4.php
+++ operadores4.php
@@ -9,22 +9,28 @@
 
 <body>
     <?php
+
+    // Operaciones de incremento/decremento
+
     $num1 = 5;
-    $num2 = 2;
+    $num2 = 10;
 
-    // Suma con operador aritmetico
-    $total = $num1 + $num2;
-    echo $total;
-
-    // Resto de una division
+    echo $num1;
     echo "<br>";
-    $resultado = $num2 % $num1;
-    echo $resultado;
 
-    // Exponencial de un numero
+    echo $num1++;
     echo "<br>";
-    $potencia = $num1 ** $num2;
-    echo $potencia;
+    echo $num1;
+
+    echo "<br>";
+    echo "<br>";
+    echo $num2;
+    echo "<br>";
+
+    echo ++$num2;
+    echo "<br>";
+    echo $num2;
+
     ?>
 </body>
 
