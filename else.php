9fe2257bd33d2d4d35e14316020fbccb3b43c194
CristiCode <cristicode12@gmail.com>
2023-11-15 17:51:02 +0100

    [+] Else

---
+++ else.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prácticas PHP</title>
+</head>
+
+<body>
+    <?php
+
+    // Estructura de control de flujo else
+
+    $edad = 12;
+
+    if ($edad >= 18) {
+        echo "Mayor de edad";
+        echo "<br>";
+    } else {
+        echo "Menor de edad";
+        echo "<br>";
+    }
+    ?>
+</body>
+
+</html>
