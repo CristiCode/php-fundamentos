d2ed1102b667e6cc31b55507d43582b2681b48a6
CristiCode <cristicode12@gmail.com>
2023-11-15 17:52:44 +0100

    [+] Operador Ternario

---
+++ ternario.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prácticas PHP</title>
+</head>
+
+<body>
+    <?php
+
+    // Estructura de control de flujo else
+
+    $nota1 = 2;
+
+    // Operador ternario
+
+    $final = $nota1 >= 5 ? "aprobado" : "suspenso";
+    echo $final;
+    ?>
+</body>
+
+</html>
