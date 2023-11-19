cfb38a03882c91fbc251630989cd98b5a7c5f965
CristiCode <cristicode12@gmail.com>
2023-11-19 22:42:05 +0100

    [+] Include

---
+++ include2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prácticas PHP</title>
+</head>
+
+<body>
+    <?php
+
+    // include2.php
+
+    echo "Soy el segundo fichero";
+    ?>
+</body>
+
+</html>
