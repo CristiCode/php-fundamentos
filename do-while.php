8cb609b1db77cd8f4fe90e8d7b56ae0b2502ead6
CristiCode <cristicode12@gmail.com>
2023-11-19 21:06:35 +0100

    [+] do-while

---
+++ do-while.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prácticas PHP</title>
+</head>
+
+<body>
+    <?php
+
+    //Estructura de control flujo do-while
+
+    $valor = 10;
+
+    while ($valor != 10) {
+        echo "dentro del while";
+    }
+
+    do {
+        echo "Dentro del do-while";
+    } while ($valor != 10);
+
+    ?>
+</body>
+
+</html>
